e320288393c72621d0f7ce1c002078001828a950
Kristopher Borel <k.borel3@gmail.com>
2017-06-14 18:34:12 +1000

    Update action creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,25 @@
-import { START_TIMER } from './constants';
+import { START_TIMER, STOP_TIMER, RESET_TIMER, TICK } from './constants';
 
 export const startTimer = () => (
-    {
-        type: START_TIMER
-    }
+  {
+    type: START_TIMER
+  }
+);
+
+export const stopTimer = () => (
+  {
+    type: STOP_TIMER
+  }
+);
+
+export const resetTimer = () => (
+  {
+    type: RESET_TIMER
+  }
+);
+
+export const tick = () => (
+  {
+    type: TICK
+  }
 );
