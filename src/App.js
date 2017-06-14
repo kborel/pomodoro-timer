7546fbfda96b1cb2a9f4016081584abe90841f22
Kristopher Borel <k.borel3@gmail.com>
2017-06-14 16:21:26 +1000

    Remove component styling
    Will focus on styling when application is functional.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import React from 'react';
-import styled from 'styled-components';
 import Timer from './components/Timer';
 
-const Div = styled.div`
-  display: grid;
-  grid-template-columns: 1fr;
-  grid-template-rows: 1fr 3fr;
-  justify-items: center;
-`;
-
-const Heading = styled.h1`
-  font-size: 50px;
-  color: deepskyblue;
-`;
-
 const App = () => (
-  <Div>
-    <Heading>Pomodoro Timer</Heading>
+  <div>
+    <h1>Pomodoro Timer</h1>
     <Timer/>
-  </Div>
+  </div>
 );
 
 export default App;
--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,39 +1,11 @@
 import React from 'react';
-import styled from 'styled-components';
-
-const Div = styled.div`
-  border-radius: 100%;
-  border: 3px solid lightgray;
-  height: 300px;
-  width: 300px;
-  padding: 75px;
-
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-around;
-`;
-
-const Text = styled.h3`
-  color: deepskyblue;
-  font-size: 75px;
-  margin: 0;
-`;
-
-const StartButton = styled.div`
-  width: 0; 
-	height: 0; 
-	border-top: 50px solid transparent;
-	border-bottom: 50px solid transparent;
-	border-left: 50px solid deepskyblue;
-  cursor: pointer;
-`;
 
 const Timer = () => (
-  <Div>
-    <Text>25:00</Text>
-    <StartButton/>
-  </Div>
+  <div>
+    25:00
+    <button>Start</button>
+    <button>Reset</button>
+  </div>
 );
 
 export default Timer;
